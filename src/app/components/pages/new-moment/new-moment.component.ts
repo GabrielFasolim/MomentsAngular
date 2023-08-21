f31c75930b6abe8e0e214d459c90212a5c568070
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2023-08-21 15:30:50 -0300

    [update] api

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Moment } from '../../../Moment';
+import { MomentService } from '../../../services/moment.service';
 
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
   styleUrls: ['./new-moment.component.css']
 })
-export class NewMomentComponent {
+export class NewMomentComponent implements OnInit {
   btnText = 'Compartilhar!';
 
+  constructor(private momentService: MomentService) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  async createHandler(moment: Moment) { 
+    const formData = new FormData();
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) { 
+      formData.append('image', moment.image);
+    }
+
+    await this.momentService.createMoment(formData).subscribe(response => {
+      
+    }, error => {
+      
+    });
+  }
 }
